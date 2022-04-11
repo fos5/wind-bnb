3dd4db147bed771cdd027dd8f59c9d4ccb32f33a
Fos5 <festusoshinfowokan@gmail.com>
2022-04-11 15:11:16 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,78 @@
 import "./App.css";
+import results from "./stays.json";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Stays from "./components/Stays";
-function App({ setishidden, ishidden, guests, location }) {
+function App() {
   const [stays, setStays] = useState([]);
+  const [ishidden, setishidden] = useState(false);
+  const [location, setlocation] = useState("Helsinki");
+
+  const helsinki = () => {
+    setlocation("Helsinki");
+  };
+  const turku = () => {
+    setlocation("Turku");
+  };
+  const oulu = () => {
+    setlocation("Oulu");
+  };
+  const vaasa = () => {
+    setlocation("Vaasa");
+  };
+  const [childAge, setchildAge] = useState(0);
+  const [adultAge, setadultAge] = useState(0);
+  // increaseAge Child;
+  const increaseChildAge = () => {
+    setchildAge((childAge) => childAge + 1);
+  };
+  // DecreaseAge Child
+  const decreaseChildAge = () => {
+    setchildAge((childAge) => childAge - 1);
+    if (childAge <= 0) {
+      return setchildAge(0);
+    }
+  }; // increaseAge Adult;
+  const increaseAdultAge = () => {
+    setadultAge((adultAge) => adultAge + 1);
+  };
+  // DecreaseAge Child
+  const decreaseAdultAge = () => {
+    setadultAge((adultAge) => adultAge - 1);
+    if (adultAge <= 0) {
+      return setadultAge(0);
+    }
+  };
+
+  // guests
+  const guests = childAge + adultAge;
 
   useEffect(() => {
     const getStays = async () => {
       const staysFromServer = await fetchStays();
-      const helsinki = staysFromServer.filter(
-        (datum) => datum.city === "Helsinki"
-      );
-      setStays(helsinki);
+      const city = staysFromServer.filter((datum) => datum.city === "Helsinki");
+      setStays(city);
     };
 
     getStays();
   }, []);
   // Fetch Stays
   const fetchStays = async () => {
-    const res = await fetch("http://localhost:5000/stays");
-    const data = await res.json();
-    return data;
-    // const helsinki = data.filter((datum) => datum.city === "Helsinki");
-    // console.log(helsinki);
+    return results;
+  };
+  const showHidden = () => {
+    setishidden(!ishidden);
   };
 
   // filter Stays
-  const filterCity = () => {
-    setishidden(!ishidden);
-    setStays(
-      stays.filter(
-        (stay) => stay.city === { location } && stay.maxGuests >= { guests }
-      )
+  const filterCity = async () => {
+    const newStaysFromServer = await fetchStays();
+    const newCity = newStaysFromServer.filter(
+      (datum) => datum.city === location && datum.maxGuests >= guests
     );
+    setStays(newCity);
+    setishidden(false);
   };
 
   // Stays lenght
@@ -41,7 +80,25 @@ function App({ setishidden, ishidden, guests, location }) {
 
   return (
     <div className="container">
-      <Navigation onFilter={filterCity} />
+      <Navigation
+        onFilter={filterCity}
+        location={location}
+        setlocation={location}
+        onHide={showHidden}
+        ishidden={ishidden}
+        setishidden={setishidden}
+        helsinki={helsinki}
+        oulu={oulu}
+        turku={turku}
+        vaasa={vaasa}
+        increaseAdultAge={increaseAdultAge}
+        adultAge={adultAge}
+        childAge={childAge}
+        increaseChildAge={increaseChildAge}
+        decreaseAdultAge={decreaseAdultAge}
+        decreaseChildAge={decreaseChildAge}
+        guests={guests}
+      />
       <Header lenght={lenght} />
       <Stays stays={stays} />
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,3 @@
-// import { keyboard } from "@testing-library/user-event/dist/keyboard";
-import { useState } from "react";
 import {
   FaMapMarkerAlt,
   FaMinus,
@@ -8,48 +6,32 @@ import {
   FaTimes,
 } from "react-icons/fa";
 import logo from "../images/logo.svg";
-const Navigation = ({ onFilter }) => {
-  const [ishidden, setishidden] = useState(false);
-  const [childAge, setchildAge] = useState(0);
-  const [adultAge, setadultAge] = useState(0);
-  const [location, setlocation] = useState("Helsinki, Finland");
-
-  // Do something
-
-  const showHidden = () => {
-    setishidden(!ishidden);
-  };
-  // increaseAge Child;
-  const increaseChildAge = () => {
-    setchildAge((childAge) => childAge + 1);
-  };
-  // DecreaseAge Child
-  const decreaseChildAge = () => {
-    setchildAge((childAge) => childAge - 1);
-    if (childAge <= 0) {
-      return setchildAge(0);
-    }
-  }; // increaseAge Adult;
-  const increaseAdultAge = () => {
-    setadultAge((adultAge) => adultAge + 1);
-  };
-  // DecreaseAge Child
-  const decreaseAdultAge = () => {
-    setadultAge((adultAge) => adultAge - 1);
-    if (adultAge <= 0) {
-      return setadultAge(0);
-    }
-  };
-  // guests
-  const guests = childAge + adultAge;
-
+const Navigation = ({
+  onFilter,
+  onHide,
+  ishidden,
+  location,
+  helsinki,
+  oulu,
+  turku,
+  vaasa,
+  increaseAdultAge,
+  increaseChildAge,
+  decreaseAdultAge,
+  decreaseChildAge,
+  adultAge,
+  childAge,
+  guests,
+}) => {
   return (
     <nav className="nav">
       <div className="logo">
-        <img src={logo} alt="logo" />
+        <a href="index.html">
+          <img src={logo} alt="logo" />
+        </a>
       </div>
-      <div className="searchBar" onClick={showHidden}>
-        <button className="btn-search city">{location}</button>
+      <div className="searchBar" onClick={onHide}>
+        <button className="btn-search city">{location}, Finland</button>
         <button className="btn-search  guests">{guests} Guests</button>
         <button className="btn-search ">
           <FaSearch color="#eb5757" />
@@ -57,35 +39,35 @@ const Navigation = ({ onFilter }) => {
       </div>
       {ishidden === true && (
         <div className="hidden-class">
-          <button onClick={showHidden} className="btn-close">
+          <button onClick={onHide} className="btn-close">
             <FaTimes color="#eb5757" />
           </button>
           <div className="hidden-class-container">
             <div className="input-bar">
               <button>
                 <span className="bold">Location</span>
-                <span className="block">{location}</span>
+                <span className="block">{location}, Finland</span>
               </button>
               <button>
                 <span className="bold">Guests</span>
                 <span className="block">{guests} guests</span>
               </button>
-              <button className="filterBtn" onClick={() => onFilter}>
+              <button onClick={onFilter} className="filterBtn">
                 <FaSearch /> Search
               </button>
             </div>
             <div className="grid-in-hidden-class">
               <div className="cities">
-                <button onClick={() => setlocation("Helsinki, Finland")}>
+                <button onClick={helsinki}>
                   <FaMapMarkerAlt /> <span>Helsinki, Finland</span>
                 </button>
-                <button onClick={() => setlocation("Turku, Finland")}>
+                <button onClick={turku}>
                   <FaMapMarkerAlt /> <span>Turku, Finland</span>
                 </button>
-                <button onClick={() => setlocation("Oulu, Finland")}>
+                <button onClick={oulu}>
                   <FaMapMarkerAlt /> <span>Oulu, Finland</span>
                 </button>
-                <button onClick={() => setlocation("Vaasa, Finland")}>
+                <button onClick={vaasa}>
                   <FaMapMarkerAlt /> <span>Vaasa, Finland</span>
                 </button>
               </div>
